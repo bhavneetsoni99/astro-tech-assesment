c2c4bee4b8fc1334e2b1e1d08d056da3aa860a35
Bhavneet <bhavneetsoni@gmail.com>
2026-07-07 15:48:31 -0500

    feat(ui): Support paginization for tables and loading feedback

--- frontend/src/components/TableComponent/TableComponent.tsx
+++ frontend/src/components/TableComponent/TableComponent.tsx
@@ -7,24 +7,30 @@ interface TableProps {
   tableName: 'players' | 'pitches';
   columns: string[];
   data: TableRow[];
+  totalCount?: number;
+  hasMoreData?: boolean;
   isLoading?: boolean;
   error?: string;
   onRowClick?: (rowId: number | string) => void;
+  onLoadMore?: () => void;
 }
 
 export const TableComponent: React.FC<TableProps> = memo(({
   tableName,
   columns,
   data,
+  totalCount = data.length,
+  hasMoreData = false,
   isLoading = false,
   error,
   onRowClick,
+  onLoadMore = () => { },
 }) => {
 
   const hasData = data.length > 0;
   const noDataMessage = `No ${tableName === 'players' ? 'players' : 'pitches'} found.`;
   const singularHeading = `1 ${tableName === 'players' ? 'Player' : 'Pitch'}`;
-  const multipleHeading = `${data.length} ${tableName === 'players' ? 'Players' : 'Pitches'}`;
+  const multipleHeading = `${totalCount} ${tableName === 'players' ? 'Players' : 'Pitches'}`;
 
   if (error) {
     return (
@@ -36,13 +42,26 @@ export const TableComponent: React.FC<TableProps> = memo(({
 
   return (
     <div className={styles.tableComponent}>
-      <h2 id="table-heading" className={!hasData ? styles.noData : ''}>
-        {hasData ? (data.length === 1 ? singularHeading : multipleHeading) : noDataMessage}
-      </h2>
-
+      <div className={styles.tableHeading}>
+        <h2 id="table-heading" className={!hasData ? styles.noData : ''}>
+          {hasData ? (data.length === 1 ? singularHeading : multipleHeading) : noDataMessage}
+        </h2>
+        {hasMoreData && (
+          <div className={styles.dataInfo}>
+            <span>Showing {data.length} of {totalCount} </span>
+            <button role="button"
+              className={styles.clickable}
+              onClick={onLoadMore}
+              onKeyDown={(e) => e.key === 'Enter' && onLoadMore()}
+            >
+              Load More
+            </button>
+          </div>
+        )}
+      </div>
       <div className={styles.tableContainer}>
         {isLoading && (<LoadingSpinner />)}
-        
+
         <table aria-labelledby="table-heading">
           <thead>
             <tr>
@@ -53,22 +72,22 @@ export const TableComponent: React.FC<TableProps> = memo(({
           </thead>
           {hasData && (
             <tbody>
-            {data.map((row, index) => (
-              <tr 
-                key={row.id} 
-                data-testid={`row-${row.id}`} 
-                className={`${index % 2 === 0 ? styles.evenRow : styles.oddRow} ${onRowClick ? styles.clickebleRow : ''}`}
-                {...(onRowClick && {
-                  tabIndex: 0,
-                  onClick: () => onRowClick(row.id),
-                  onKeyDown: (e: React.KeyboardEvent<HTMLTableRowElement>) => e.key === 'Enter' && onRowClick(row.id),
-                })}
-              >
-                {row.cells.map((cell, cellIndex) => (
-                  <td key={columns[cellIndex]}>{cell}</td>
-                ))}
-              </tr>
-            ))}
+              {data.map((row, index) => (
+                <tr
+                  key={row.id}
+                  data-testid={`row-${row.id}`}
+                  className={`${index % 2 === 0 ? styles.evenRow : styles.oddRow} ${onRowClick ? styles.clickable : ''}`}
+                  {...(onRowClick && {
+                    tabIndex: 0,
+                    onClick: () => onRowClick(row.id),
+                    onKeyDown: (e: React.KeyboardEvent<HTMLTableRowElement>) => e.key === 'Enter' && onRowClick(row.id),
+                  })}
+                >
+                  {row.cells.map((cell, cellIndex) => (
+                    <td key={columns[cellIndex]}>{cell}</td>
+                  ))}
+                </tr>
+              ))}
             </tbody>
           )}
         </table>
