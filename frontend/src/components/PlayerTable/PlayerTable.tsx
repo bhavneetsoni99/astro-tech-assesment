a21c8aeb6eaf7a5c5443e6c332585823056dae77
Bhavneet <bhavneetsoni@gmail.com>
2026-07-12 21:00:51 -0500

    chore: remove unused variables

--- frontend/src/components/PlayerTable/PlayerTable.tsx
+++ frontend/src/components/PlayerTable/PlayerTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import ApiService from "../../services/api";
 import { Player, TableRow } from "../../types";
